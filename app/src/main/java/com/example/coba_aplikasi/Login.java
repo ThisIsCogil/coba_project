ef9f9c6beda79b7558133e42a78180b5565b0bb8
ThisIsCogil <yadam7195@gmail.com>
2024-10-06 19:57:37 +0700

    UPDATE getExtras dan Shared Preferences

--- app/src/main/java/com/example/coba_aplikasi/Login.java
+++ app/src/main/java/com/example/coba_aplikasi/Login.java
@@ -1,14 +1,21 @@
 package com.example.coba_aplikasi;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
 public class Login extends AppCompatActivity {
+    private TextInputEditText edtuser, edtpass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,13 +23,31 @@ public class Login extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
 
+        edtuser = findViewById(R.id.edit_user);
+        edtpass = findViewById(R.id.edittext_pass);
+        Intent intent = getIntent();
+        String username = intent.getStringExtra("username");
+        edtuser.setText(username);
         Button btnlog = findViewById(R.id.btnlogin);
+
+
         btnlog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String inputUsername = edtuser.getText().toString();
+                String inputPassword = edtpass.getText().toString();
 
-                Intent intent = new Intent(Login.this, Dashboard.class);
-                startActivity(intent);
+                SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+                String savedUsername = sharedPreferences.getString("username", null);
+                String savedPassword = sharedPreferences.getString("password", null);
+
+                if (inputUsername.equals(savedUsername) && inputPassword.equals(savedPassword)) {
+                    Intent intent = new Intent(Login.this, Dashboard.class);
+                    startActivity(intent);
+                }
+                else {
+                    Toast.makeText(Login.this, "Username atau Password salah", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
--- app/src/main/java/com/example/coba_aplikasi/Register.java
+++ app/src/main/java/com/example/coba_aplikasi/Register.java
@@ -1,9 +1,11 @@
 package com.example.coba_aplikasi;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,9 +13,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 
 public class Register extends AppCompatActivity {
+
+    private TextInputEditText edtemail, edtuser, edtpass;
 
 
     @Override
@@ -22,12 +28,27 @@ public class Register extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
 
+        edtemail = findViewById(R.id.edit_email);
+        edtuser = findViewById(R.id.edit_user);
+        edtpass = findViewById(R.id.edittext_pass);
         Button btnreg = findViewById(R.id.btnregister);
+
         btnreg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String email = edtemail.getText().toString();
+                String username = edtuser.getText().toString();
+                String password = edtpass.getText().toString();
+
+                SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("email", email);
+                editor.putString("username", username);
+                editor.putString("password", password);
+                editor.apply();
 
                 Intent intent = new Intent(Register.this, Login.class);
+                intent.putExtra("username", username);
                 startActivity(intent);
             }
         });
